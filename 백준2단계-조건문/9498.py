4dd972eaa26d40b8756741e88301802e3bf9e9d8
keum <keumdy0@gmail.com>
2023-03-20 20:29:46 +0900

    백준2단계-조건문2

---
+++ 백준2단계-조건문/9498.py
@@ -0,0 +1,11 @@
+A = input()
+if int(A) >= 90 :
+    print('A')
+elif int(A) >= 80 :
+    print('B')
+elif int(A) >= 70 :
+    print('C')
+elif int(A) >= 60 :
+    print('D')
+else :
+    print('F')
