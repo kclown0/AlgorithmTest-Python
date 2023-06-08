695952463d1c53e8462e0a8db976c1d7814ac86e
kclown0 <keumdy0@gmail.com>
2023-06-08 17:38:54 +0900

    feat: Create 배열 뒤집기.py

---
+++ programmers/Lv.0/배열 뒤집기.py
@@ -0,0 +1,8 @@
+def solution(num_list):
+    # num_list = num_list[::-1]
+    # num_list.reverse()
+    answer = list(reversed(num_list))
+    return answer
+
+# num_list=[1,2,3,4]
+# print(solution(num_list))
